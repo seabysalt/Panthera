6ab6165196e3d7b20a1aa514ec70602c20e7eeee
seabysalt <seabysalt@gmail.com>
2019-07-11 11:42:33 +0200

    updated session.js with optimized video activation/deactivation

--- routes/session.js
+++ routes/session.js
@@ -58,75 +58,73 @@ router.get('/profile/deleteInterest/:interestId', (req, res) => {
 
 router.get("/home", ensureLogin.ensureLoggedIn(), (req, res) => {
 
-    // let interestsFeed = [...req.user.interests].map(el => getArticlesForInterest(el, 'en'))
+    // Beginning of activation for video
 
-    // let mediumFeed = [...req.user.interests].map(el => getRSS("https://medium.com/feed/tag/", el));
+    // let youtubeFeed = [...req.user.interests].map(el => youtube.search.list({
+    //    part: "snippet",
+    //    maxResults: 1,
+    //    q: 'tedx+'+el.split('').join('+'), 
+    // //    videoDefinition: "standard",
+    //  }).then(response => {
+    //      console.log(response.data.items)
+    //        return response.data.items.map(el=> ({videoId: el.id.videoId, title: el.snippet.title}))
+    //  }).catch(err => {
+    //    console.log(err);
+    //  })
+    //   ) 
 
-    let youtubeFeed = [...req.user.interests].map(el => youtube.search.list({
-       part: "snippet",
-       maxResults: 1,
-       q: 'tedx+'+el.split('').join('+'), 
-    //    videoDefinition: "standard",
-     }).then(response => {
-         console.log(response.data.items)
-           return response.data.items.map(el=> ({videoId: el.id.videoId, title: el.snippet.title}))
-     }).catch(err => {
-       console.log(err);
-     })
-      ) 
-
-    // Promise.all(mediumFeed).then((mediumData) => {
-       // get the video ids rs
-
-    Promise.all(youtubeFeed).then((videoObj) => {
+    // Promise.all(youtubeFeed).then((videoObj) => {
  
-        let newObj = videoObj.reduce((acc,val)=>acc.concat(val),[]).filter(el=>el.videoId != undefined)
+    //     let newObj = videoObj.reduce((acc,val)=>acc.concat(val),[]).filter(el=>el.videoId != undefined)
  
-        let interestsFeed = [...req.user.interests].map(el => getArticlesForInterest(el, 'en'))
-         function shuffle(a) {
-         for (let i = a.length - 1; i > 0; i--) {
-             const j = Math.floor(Math.random() * (i + 1));
-             [a[i], a[j]] = [a[j], a[i]];
-            }
-         return a;
-        }
+    //     let interestsFeed = [...req.user.interests].map(el => getArticlesForInterest(el, 'en'))
+    //      function shuffle(a) {
+    //      for (let i = a.length - 1; i > 0; i--) {
+    //          const j = Math.floor(Math.random() * (i + 1));
+    //          [a[i], a[j]] = [a[j], a[i]];
+    //         }
+    //      return a;
+    //     }
  
-     Promise.all(interestsFeed)
-         .then(feed => {
-             const feedArticles = (feed.reduce((acc, val) => {
-             return acc.concat(val.articles)
-             }, []))
-         res.render("session/home", { user: req.user, videoObj:newObj, feedArticles: shuffle(feedArticles).splice(0, 10)});
-     }).catch(err => {
-         console.log(err)
-     })
-        })
-    });
+    //  Promise.all(interestsFeed)
+    //      .then(feed => {
+    //          const feedArticles = (feed.reduce((acc, val) => {
+    //          return acc.concat(val.articles)
+    //          }, []))
+    //      res.render("session/home", { user: req.user, videoObj:newObj, feedArticles: shuffle(feedArticles).splice(0, 10)});
+    //  }).catch(err => {
+    //      console.log(err)
+    //  })
+    //     })
+    // });
+
+    // End of thing to activation for video
+
 
     //  Activate this if you want to de-activate loading videos:
 
-//     let interestsFeed = [...req.user.interests].map(el => getArticlesForInterest(el, 'en'))
-//     function shuffle(a) {
-//         for (let i = a.length - 1; i > 0; i--) {
-//             const j = Math.floor(Math.random() * (i + 1));
-//             [a[i], a[j]] = [a[j], a[i]];
-//         }
-//         return a;
-//     }
+    let interestsFeed = [...req.user.interests].map(el => getArticlesForInterest(el, 'en'))
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
 
-//     Promise.all(interestsFeed)
-//         .then(feed => {
-//             const feedArticles = (feed.reduce((acc, val) => {
-//                 return acc.concat(val.articles)
-//             }, []))
-//             res.render("session/home", { user: req.user, feedArticles: shuffle(feedArticles).splice(0, 10) });
-//         }).catch(err => {
-//             console.log(err)
-//         })
+    Promise.all(interestsFeed)
+        .then(feed => {
+            const feedArticles = (feed.reduce((acc, val) => {
+                return acc.concat(val.articles)
+            }, []))
+            res.render("session/home", { user: req.user, feedArticles: shuffle(feedArticles).splice(0, 10) });
+        }).catch(err => {
+            console.log(err)
+        })
 
-//  });
+ });
 
-
+// Active until here for only articles
 
 
 //######### LIKE BUTTON, | CREATE POST IN DB or IF ALREADY IN DB PUSH USER ID IN 'likedBy' #########
